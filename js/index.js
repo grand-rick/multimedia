9d1b1878c26c953249feab20b343cc4e6b26d1ee
Patrick Kabuga <kabugap02@gmail.com>
2023-04-07 15:03:41 +0300

    [fix]: Add responsiveness

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function tree(){
-    document.getElementById("plant").innerHTML=""; 
-}
-function animal(){
-    document.getElementById("leo").innerHTML="Leopard is an animal that belong to genius Panthera and species name leo."+
-    "Therefore its scientific name is Panthera leo.It is mostly found in sub-Saharan Africa, Western and central Asia";
-}
